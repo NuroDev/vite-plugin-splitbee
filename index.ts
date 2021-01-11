cb8289d1b9d47b8ac3185164feeaef13e9140557
N U R O ™ <me@nuro.dev>
2021-01-11 11:25:16 +0000

    Added async argument
    Allows for toggling of the initial content flash

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import type { Plugin, ResolvedConfig } from 'vite';
 
 export interface SplitbeeOptions {
+	/// When using A/B testing, you can prevent the initial content flash by making the script blocking/synchronous (Optional) [Default: false]
+	async: boolean;
 	/// Whether to include Splitbee analytics during local development (Optional) [Default: false]
 	enable_development: boolean;
 	/// Number of milliseconds to wait before loading the Splitbee (Optional) [Default: 0]
@@ -9,7 +11,7 @@ export interface SplitbeeOptions {
 
 export const ViteSplitbee = (options?: SplitbeeOptions): Plugin => {
 	const enable_development = !!(options?.enable_development);
-	const script_src: string = `(function(){var w=window;var sb=w.splitbee;var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Splitbee=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://cdn.splitbee.io/sb.js';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}function ld(){setTimeout(l, ${options?.delay_timeout || 0});}if(w.attachEvent){w.attachEvent('onload',ld);}else{w.addEventListener('load',ld,false);}})()`;
+	const script_src: string = `(function(){var w=window;var sb=w.splitbee;var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Splitbee=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=${options?.async || true};s.src='https://cdn.splitbee.io/sb.js';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}function ld(){setTimeout(l, ${options?.delay_timeout || 0});}if(w.attachEvent){w.attachEvent('onload',ld);}else{w.addEventListener('load',ld,false);}})()`;
 
 	let viteConfig: ResolvedConfig | undefined;
 
