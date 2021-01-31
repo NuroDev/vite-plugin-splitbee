ae551ec1d7c0bd99ba5da5bfadbbfa0ee73494f4
N U R O ™ <me@nuro.dev>
2021-01-31 15:10:07 +0000

    Updated configuration options

--- index.ts
+++ index.ts
@@ -2,16 +2,23 @@ import type { Plugin, ResolvedConfig } from 'vite';
 
 export interface SplitbeeOptions {
 	/// When using A/B testing, you can prevent the initial content flash by making the script blocking/synchronous (Optional) [Default: false]
-	async: boolean;
+	async?: boolean;
+
 	/// Whether to include Splitbee analytics during local development (Optional) [Default: false]
-	enable_development: boolean;
+	dev?: boolean;
+	
 	/// Number of milliseconds to wait before loading the Splitbee (Optional) [Default: 0]
-	delay_timeout: number;
+	delay_timeout?: number;
 };
 
-export const ViteSplitbee = (options?: SplitbeeOptions): Plugin => {
-	const enable_development = !!(options?.enable_development);
-	const script_src: string = `(function(){var w=window;var sb=w.splitbee;var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Splitbee=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=${options?.async || true};s.src='https://cdn.splitbee.io/sb.js';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}function ld(){setTimeout(l, ${options?.delay_timeout || 0});}if(w.attachEvent){w.attachEvent('onload',ld);}else{w.addEventListener('load',ld,false);}})()`;
+export const ViteSplitbee = (options: SplitbeeOptions = {}): Plugin => {
+	const {
+		async = true,
+		dev = false,
+		delay_timeout = 0,
+	} = options;
+
+	const script_src: string = `(function(){var w=window;var sb=w.splitbee;var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Splitbee=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=${async};s.src='https://cdn.splitbee.io/sb.js';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}function ld(){setTimeout(l, ${delay_timeout});}if(w.attachEvent){w.attachEvent('onload',ld);}else{w.addEventListener('load',ld,false);}})()`;
 
 	let viteConfig: ResolvedConfig | undefined;
 
@@ -23,7 +30,7 @@ export const ViteSplitbee = (options?: SplitbeeOptions): Plugin => {
 		transformIndexHtml: {
 			enforce: 'post',
 			transform(html: any) {
-				if (!viteConfig?.isProduction && !enable_development) return html;
+				if (!viteConfig?.isProduction && !dev) return html;
 
 				// Inject the Splitbee script at the end of the `<head>`
 				return html.replace(
